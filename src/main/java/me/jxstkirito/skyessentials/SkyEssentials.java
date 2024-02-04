9171d377216245e7695699d4e6becda6795b9647
Lasse Gröne <lassegrone@MacBook-Pro-von-Lasse.fritz.box>
2024-02-04 14:50:47 +0100

    Initial commit

---
+++ src/main/java/me/jxstkirito/skyessentials/SkyEssentials.java
@@ -0,0 +1,17 @@
+package me.jxstkirito.skyessentials;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyEssentials extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
